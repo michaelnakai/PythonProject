6a29a40cd6ae1dd5001ad960683b7ab2a06ef0ff
Michael Nakai <michaelnakai@weber.edu>
2018-11-13 19:12:23 -0700

    Welcome! The first commit!

---
+++ print.py
@@ -0,0 +1,45 @@
+# Demonstration of the print statement
+# Other information
+# print("Hello World")
+# print('Hello World')
+
+# print("I can't do it")
+# print('Michael sure "tries"')
+
+# # Escape Characters
+# print('This is the first line \nThis is the second line')
+
+# # print integer and an integer string
+# print(35)
+# print('35')
+
+# # Concatenation combining strings
+# firstname = "Michael"   # this is an inline comment
+# lastname  = "Nakai"     # this stores the users last name
+
+# print(firstname + " " + lastname)
+
+#     # other options
+# print (firstname,lastname)
+
+# print ('{0} first Name {1} last name'.format(firstname, lastname))
+# print ('{1} first Name {0} last name'.format(firstname, lastname))
+
+# # printing integers
+# firstnumber = 5
+# secondnumber = 10
+# print(firstnumber + secondnumber)
+# print(firstnumber, secondnumber)
+# print('{0} is greater than {1}'.format(firstnumber, secondnumber))
+
+# perform math function
+# floating number
+hightestscore = 0.9372 # floating point number
+lowtestscore = 0.4598 # floating point number
+print('The high score was ' + str(hightestscore) + '\nthe low test score was ' + str(lowtestscore))
+print('The high score was {0:.2f}\nthe low test score was {1:.2f}'.format(hightestscore,lowtestscore))
+
+print ('The print a list of things\n'
+        'Apple\n'
+        'Banana\n'
+        'Orange\n')
